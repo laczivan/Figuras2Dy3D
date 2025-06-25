54caf31ba4a8c65a6494fb1835a9a41ef877e3f9
laczivan <l.cazl041023@itses.edu.mx>
2025-06-25 16:55:49 -0600

    Update Circulo.java

--- src/main/java/mx/edu/itses/electricidad/iz/figuras2d_3d/Circulo.java
+++ src/main/java/mx/edu/itses/electricidad/iz/figuras2d_3d/Circulo.java
@@ -3,9 +3,15 @@ package mx.edu.itses.electricidad.iz.figuras2d_3d;
 
 
 public class Circulo {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
+private double radio;
+    public Circulo(double radio){
+        this.radio=radio;
     }
+    public double calcularArea(){
+        return Math.PI*Math.pow(radio,2);   
+    }
+    public calcularPerimetro(){
+        return 2*Math.PI*radio;
+    
+        }
 }
